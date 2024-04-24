70e2abd602292094104d7af46f926fb521595f7f
CHThia <capitoljazzakat@gmail.com>
2024-04-24 11:20:36 +0800

    feat: Webform for Create New Deck

--- src/pages/CreateNewDeck.jsx
+++ src/pages/CreateNewDeck.jsx
@@ -1,11 +1,40 @@
+import { useState, useEffect } from 'react';
 
 export default function CreateNewDeck (){
 
+  const [searchCard, setSearchCard] = useState([]);
+
   return (
     <>
-      <div className="title">
-        <h1>Create New Deck</h1>
-      </div>
+      {/* Create New Deck Section */}
+      <webform>
+
+        <fieldset className='fieldset-container'>
+          <legend>Create New Deck</legend>
+            <div>
+              <div id="detail-input">
+                <label>Deck Name: </label>
+                <input type='text' style={{width:"100%"}}></input>
+              </div>
+
+              <div id="detail-input">
+                <label>Create Date: </label>
+                <input type='date' pattern="\d{2}-\d{2}-\d{4}" style={{width:"100%"}}></input>
+              </div>
+
+              <div id="detail-input">
+                <label>Search Card: </label>
+                <input type='search' style={{width:"100%"}}></input>
+              </div>
+            </div>
+
+            <div id='show-card'></div>
+      
+        </fieldset>
+      </webform>
+
+      
+
     </>
   )
 }

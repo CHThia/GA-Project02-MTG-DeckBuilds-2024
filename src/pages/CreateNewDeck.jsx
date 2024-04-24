5d8d7d19a0463b64b4c77194c7177732ca716490
CHThia <capitoljazzakat@gmail.com>
2024-04-24 12:42:15 +0800

    Feat: Create 60 card slots for deck builds

--- src/pages/CreateNewDeck.jsx
+++ src/pages/CreateNewDeck.jsx
@@ -1,39 +1,52 @@
-import { useState, useEffect } from 'react';
+import { useState} from 'react';
+
+
 
 export default function CreateNewDeck (){
 
   const [searchCard, setSearchCard] = useState([]);
 
+  // Create 60 slots for placing cards 
+  const deckTable = [];
+  for(let i = 0; i < 60; i++){
+    deckTable.push(<div key={i} className='card-slot'>Empty Slot</div>)
+  }
+
+
   return (
     <>
-      {/* Create New Deck Section */}
-      <webform>
+      <div className='main-body'>
 
-        <fieldset className='fieldset-container'>
-          <legend>Create New Deck</legend>
-            <div>
-              <div id="detail-input">
-                <label>Deck Name: </label>
-                <input type='text' style={{width:"100%"}}></input>
+        {/* Create New Deck Section */}
+        <webform>
+
+          <fieldset className='fieldset-container'>
+            <legend>Create New Deck</legend>
+              <div>
+                <div id="detail-input">
+                  <label>Deck Name: </label>
+                  <input type='text' style={{width:"100%"}}></input>
+                </div>
+
+                <div id="detail-input">
+                  <label>Create Date: </label>
+                  <input type='date' pattern="\d{2}-\d{2}-\d{4}" style={{width:"100%"}}></input>
+                </div>
+
+                <div id="detail-input">
+                  <label>Search Card: </label>
+                  <input type='search' style={{width:"100%"}}></input>
+                </div>
               </div>
 
-              <div id="detail-input">
-                <label>Create Date: </label>
-                <input type='date' pattern="\d{2}-\d{2}-\d{4}" style={{width:"100%"}}></input>
-              </div>
+              <div id='show-card'></div>
+        
+          </fieldset>
+        </webform>
 
-              <div id="detail-input">
-                <label>Search Card: </label>
-                <input type='search' style={{width:"100%"}}></input>
-              </div>
-            </div>
+        <div className='create-deck-container'>{deckTable}</div>
 
-            <div id='show-card'></div>
-      
-        </fieldset>
-      </webform>
-
-      
+      </div>
 
     </>
   )
